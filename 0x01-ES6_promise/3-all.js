3767ecaf1a5fc44acdced253c7cc5d19a7524c34
dr.omartalat <dr.omartalat@gmail.com>
2024-10-02 19:52:12 +0300

    modified:   0x01-ES6_promise/3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(
-      (values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`),
-    )
-    .catch(() => console.error('Signup system offline'));
+    .then((values) => {
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+    }).catch(() => console.log('Signup system offline'));
 }
