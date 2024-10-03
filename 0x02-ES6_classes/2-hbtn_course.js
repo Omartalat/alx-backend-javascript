4283d6b226c65466e94469f09f5d3bed01bf3ccf
dr.omartalat <dr.omartalat@gmail.com>
2024-10-03 21:28:33 +0300

    classes done

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,55 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    if (
+      !Array.isArray(students)
+      || !students.every((item) => typeof item === 'string')
+    ) {
+      throw TypeError('Students must be an array of strings');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  set students(value) {
+    if (
+      !Array.isArray(value)
+      || !value.every((item) => typeof item === 'string')
+    ) {
+      throw TypeError('Students must be an array of strings');
+    }
+    this._students = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
