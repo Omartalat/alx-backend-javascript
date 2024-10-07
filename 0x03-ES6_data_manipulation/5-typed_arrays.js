2358fd14b710c286e671fcc5b74b98622616292a
dr.omartalat <dr.omartalat@gmail.com>
2024-10-07 23:55:30 +0300

    task 4 modified task 5 done

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
   const dv = new DataView(new ArrayBuffer(length));
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
   dv.setUint8(position, value);
   return dv;
 }
